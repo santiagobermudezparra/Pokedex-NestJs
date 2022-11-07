8648fb7cb24fd7cfe3e3c98700e1728c56f53c5a
Santiago Bermudez <santiagobermudezparra@gmail.com>
2022-11-07 22:18:02 +1300

    Post

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+      }),
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+  ],
+})
+export class AppModule {}
